4df071ab7e0dca542c2182c5cc29d66ed0bc0761
AbasSec <abasmust277@gmail.com>
2026-07-03 17:10:34 +0300

    I changed this

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-export default function Home() { return <h1>FLOW is running!</h1>; }
+export default function Home() {
+  return (
+    <main>
+      <h1>FLOW is running!</h1>
+      <p>Operational system for F&amp;B teams.</p>
+    </main>
+  );
+}
